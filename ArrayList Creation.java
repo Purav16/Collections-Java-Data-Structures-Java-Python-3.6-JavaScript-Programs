68583f0b8ebdecb0a63d1ab73dc67241b64963e1
Purav16 <puravdesai57@gmail.com>
2018-01-20 00:04:49 -0800

    Creating ArrayList Code

---
+++ ArrayList Creation.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Array_List_Creation {
+  public static void main(String[] args) {
+  List<String> colours = new ArrayList<String>();
+  colours.add("Red");
+  colours.add("Green");
+  colours.add("Orange");
+  colours.add("White");
+  colours.add("Black");
+  System.out.println(colours);
+  
+  List<Integer> colour_code = new ArrayList<Integer>();
+  colour_code.add(1);
+  colour_code.add(2);
+  colour_code.add(3);
+  colour_code.add(4);
+  colour_code.add(5);
+  System.out.println(  colour_code);
+
+ }
+}
