ea132a8ee9ea51db2a8c2e5a37989a063515a73a
Purav Desai <puravdesai57@gmail.com>
2018-02-05 10:13:12 -0800

    Python list remove methods

---
+++ Python list Pop and Remove methods.py
@@ -0,0 +1,15 @@
+#initializing a pythn list with some elements
+list1 = [1,2,3,4,5,6,7,8,9,10]
+
+#Remving a value known element
+list1.remove(1)
+list1.remove(10)
+print(list1)
+
+#Removing last element using POP method
+list1.pop()
+print(list1)
+
+#Removing more than 1 elements using pop
+list1.pop(2)
+print(list1)
