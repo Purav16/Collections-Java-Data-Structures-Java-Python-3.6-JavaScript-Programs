ec3bd78a82bbe23291572fba5f25dbaed1fcc1a0
Purav Desai <puravdesai57@gmail.com>
2018-02-06 11:39:53 -0800

    python set

---
+++ Sets_Duplication_Python.py
@@ -0,0 +1,10 @@
+#Python Sets
+set_1 = {'One' , 'Two' , 'Three' , 'Four'}
+print_set={x for x in set_1}
+print(print_set,1)
+
+#Duplication removal/not allowed in Sets
+a = set('vslvnlsndlsvSHVJskdslhsjksaa')
+print(a)
+
+
