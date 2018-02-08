788cd903c971ec6c8623c205d594855bba881f43
Purav Desai <puravdesai57@gmail.com>
2018-02-08 12:08:01 -0800

    Python Enum

---
+++ Enum_Iteration.py
@@ -0,0 +1,9 @@
+from my_enum import Enum
+class Country(Enum):
+    Albania = 355
+    Algeria = 213
+    Andorra = 376
+    Angola = 244
+    Antarctica = 672
+for data in Country:
+    print('{:15} = {}'.format(data.name, data.value))
