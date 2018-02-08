789414401e4676c864f7482f526dfd4fae891a07
Purav Desai <puravdesai57@gmail.com>
2018-02-08 12:09:30 -0800

    Python Enum display all records

---
+++ Enum_All.py
@@ -0,0 +1,14 @@
+from my_enum import Enum
+class Country(Enum):
+    Albania = 355
+    Algeria = 213
+    Andorra = 376
+    Angola = 244
+    Antarctica = 672
+for data in Country:
+    print('{:15} = {}'.format(data.name, data.value))
+print('Country Name ordered by Country Code:')
+print('\n'.join('  ' + c.name for c in sorted(Country)))
+
+country_code_list = list(map(int, Country))
+print(country_code_list)
